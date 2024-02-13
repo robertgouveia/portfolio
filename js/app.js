c5973444683747f278e47d2c5b65208ffa4f83df
Robert Gouveia <robertgouveia03@hotmail.com>
2024-02-13 19:26:23 +0000

    Complete Mobile Remake

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
-const nav = document.querySelector('.fa-bars')
-const overlay = document.querySelector('.overlay')
-const content = document.querySelector('.content')
-nav.addEventListener('click', () => {
-  if(nav.classList.contains('active')){
-    nav.classList.remove('active')
-    nav.classList.add('fa-bars')
-    nav.classList.remove('fa-x')
-    overlay.classList.remove('active')
-    content.classList.remove('active')
+const menu = document.querySelector('.menu')
+const mobile_menu = document.querySelector('.toggle_menu')
+
+let menu_toggle = false
+
+menu.addEventListener('click', () => {
+  if(menu.classList.contains('fa-bars')){
+    menu.classList.remove('fa-bars')
+    menu.classList.add('fa-x')
   } else {
-    nav.classList.add('active')
-    nav.classList.remove('fa-bars')
-    nav.classList.add('fa-x')
-    overlay.classList.add('active')
-    setTimeout(function () {
-      content.classList.add('active')
-      overlay.style.display = 'flex'
-    }, 500);
+    menu.classList.add('fa-bars')
+    menu.classList.remove('fa-x')
   }
+  menu.classList.add('active')
+  menu_toggle ? mobile_menu.classList.remove('active') : mobile_menu.classList.add('active')
+  menu_toggle = !menu_toggle
 })
