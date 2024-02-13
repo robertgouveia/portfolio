007a571ff857fcbf299e200ee49e9816fe950807
Robert Gouveia <robertgouveia03@hotmail.com>
2024-02-13 12:37:13 +0000

    Added JS triggers and mobile functionality.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('.fa-bars')
+const overlay = document.querySelector('.overlay')
+const content = document.querySelector('.content')
+nav.addEventListener('click', () => {
+  if(nav.classList.contains('active')){
+    nav.classList.remove('active')
+    nav.classList.add('fa-bars')
+    nav.classList.remove('fa-x')
+    overlay.classList.remove('active')
+    content.classList.remove('active')
+
+  } else {
+    nav.classList.add('active')
+    nav.classList.remove('fa-bars')
+    nav.classList.add('fa-x')
+    overlay.classList.add('active')
+    setTimeout(function () {
+      content.classList.add('active')
+    }, 500);
+  }
+})
